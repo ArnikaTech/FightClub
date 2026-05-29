943c016ffa8dc4d4bb92668c05b0253db5f3cb88
Hamid Reza Bardarani <little4uth0r@gmail.com>
2026-05-29 12:24:55 +0330

    Add contact modal to student list, attendance, absentees - fix card overflow - avatar fix

--- apps/students/templatetags/dict_filters.py
+++ apps/students/templatetags/dict_filters.py
@@ -1,4 +1,7 @@
 from django import template
+
+import json
+
 register = template.Library()
 
 @register.filter
@@ -6,3 +9,7 @@ def get_item(dictionary, key):
     if dictionary:
         return dictionary.get(key)
     return None
+
+@register.filter
+def safe_json(data):
+    return json.dumps([{'phone': c.phone, 'label': c.label or c.get_contact_type_display()} for c in data], ensure_ascii=False)
